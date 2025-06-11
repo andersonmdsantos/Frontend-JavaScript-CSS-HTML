9f3cd5c9e280d901019c23e0e1c6f857b079b81f
Anderson Santos <88168152+andersonmdsantos@users.noreply.github.com>
2025-06-11 18:37:35 -0300

    Adição de um exercício de variaveis em JavaScript

---
+++ Aula02-Declarando-Variaveis/Declaring-Variables.js
@@ -0,0 +1,14 @@
+// let const
+
+const primeiroValor = 10;
+const segundoValor = 20;
+
+console.log("PrimeiroValor + segundoValor: ", primeiroValor + segundoValor);
+const valorTotal = primeiroValor + segundoValor;
+console.log("ValorTotal: ",valorTotal);
+
+let terceiroValor = 50;
+console.log("terceiroValor: ", terceiroValor);
+
+terceiroValor = 100;
+console.log("terceiroValor final: ", terceiroValor);
