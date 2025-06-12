04116bf74e34280478e2b07a113b4ef39b401392
Anderson Santos <88168152+andersonmdsantos@users.noreply.github.com>
2025-06-12 20:30:46 -0300

    Fim da aula03

---
+++ Aula03-Tipo-De-Dados/dataTypesObject.js
@@ -0,0 +1,34 @@
+// const fichaDoAnderson = {
+//     nome: "Anderson",
+//     idade: 28,
+//     profissao: "Engenheiro",
+//     temFilhos: false,
+// };
+
+// console.log("Ficha do Anderson:", fichaDoAnderson);
+
+// console.log("Nome: " + fichaDoAnderson.nome);
+
+// fichaDoAnderson.carro = "Escort";
+
+// console.log("Ficha do Anderson", fichaDoAnderson);
+
+// delete fichaDoAnderson.carro;
+
+// console.log("Fica do Anderson", fichaDoAnderson);
+
+// console.log("Carro: " + fichaDoAnderson.carro);
+
+const nome = "Anderson";
+const idade = 28;
+const profissao = "Engenheiro";
+const temFilhos = false;
+
+const fichaDoAnderson = {
+    nome,
+    idade,
+    profissao,
+    temFilhos,
+};
+
+console.log("Ficha do Anderson", fichaDoAnderson);
